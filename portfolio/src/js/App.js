6f379b383b0c705560c759325240815861618794
AnuragBetageri <126987627+AnuragBetageri@users.noreply.github.com>
2023-06-23 04:45:58 +0530

    fixed ui issues

--- portfolio/src/js/App.js
+++ portfolio/src/js/App.js
@@ -10,14 +10,21 @@ import 'aos/dist/aos.css'
 
 window.addEventListener("load", isMobileDevice);
 
+var lineMul = 1;
 function isMobileDevice() {
+  
   document.body.style.zoom = "1";
   return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 }
 
 // Show alert message only on phones
 if (isMobileDevice()) {
+  lineMul = 1.27;
   alert("This page is under development for mobile so it is best viewed in PC");
+}
+else
+{
+  lineMul = 1;
 }
 
 
@@ -27,7 +34,7 @@ function thelineani()
 {
   var line = document.getElementsByClassName("the_line")[0];
   var aboutmeSec = document.getElementById("AboutSec")
-  var line_hight = (window.scrollY - 30)
+  var line_hight = ((window.scrollY - 30)*lineMul)
   line.style.height = line_hight + "px"
   // if(line.getBoundingClientRect().bottom > aboutmeSec.getBoundingClientRect().top )
   // {
