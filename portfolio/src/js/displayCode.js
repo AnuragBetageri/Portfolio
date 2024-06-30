c9ab97f1f6586b04f2c955382aa6b57185dd5b21
AnuragBetageri <126987627+AnuragBetageri@users.noreply.github.com>
2024-06-30 18:59:52 +0530

    added run code

--- portfolio/src/js/displayCode.js
+++ portfolio/src/js/displayCode.js
@@ -19,7 +19,8 @@ function DisplayCode() {
             {isModalOpen && (
                 <div className="modal">
                     <div className="modal-content">
-                        <span className="close" onClick={closeModal}>&times;</span>
+                        <div className='close_bar'><span className="close" onClick={closeModal}>&times;</span></div>
+                        <div className='output'>
                         <p>My Details:</p>
                         <p>Name: Anurag C Betageri</p>
                         <p>Age: 22</p>
@@ -29,16 +30,20 @@ function DisplayCode() {
                         <p>University: Reva University, Bangalore, India</p>
                         <p>Course/Board: B Tech Computer Science</p>
                         <p>Year: 2024</p>
+                        <br></br>
                         <p>PreUniversity:</p>
                         <p>College: Mahesh PU College, Mangalore, India</p>
                         <p>Course/Board: PCMC in Karnataka Board</p>
                         <p>Year: 2020</p>
+                        <br></br>
                         <p>Schooling:</p>
                         <p>School: Bhuvana Jyothi Residential School, Shirthady, Karnataka</p>
                         <p>Course/Board: CBSE</p>
                         <p>Year: 2018</p>
+                        <br></br>
                         <p>Hobbies: Painting, Testing custom Android OS, and Testing games</p>
                         <p>=== Thank You ===</p>
+                        </div>
                     </div>
                 </div>
             )}
